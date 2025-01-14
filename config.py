7f1aaad65ad486c7c32828bb13751e5ef8c8520d
Michiel Rogissart <michiel@datavillage.me>
2025-01-14 09:10:53 +0100

    use DV_CAGE_ID instead of DV_APP_ID

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 ALGO_VERSION = '0.0.1'
 DV_TOKEN = os.environ['DV_TOKEN']
 CONTROL_PLANE_URL = os.environ['CONTROL_PLANE_URL']
-DV_APP_ID = os.environ['DV_APP_ID']
+DV_CAGE_ID = os.environ['DV_CAGE_ID']
 
 CONNECTOR_FILE_NAME="gcs_netflix"
 CONNECTOR_FILE_DIR="scrapbook"
