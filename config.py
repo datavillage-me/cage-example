638c41db69c44a204f72b54ac06ee8f08dc28601
Michiel Rogissart <michiel@datavillage.me>
2025-01-17 13:11:37 +0100

    create DATA_FOLDER directories if not exists

--- config.py
+++ config.py
@@ -2,8 +2,9 @@ import os
 
 # DATA_FOLDER="data/input"
 DATA_FOLDER="/resources/data"
+os.makedirs(DATA_FOLDER, exist_ok=True)
 
-ALGO_VERSION = '0.4.2rc1'
+ALGO_VERSION = '0.4.2rc2'
 CONTROL_PLANE_URL = os.environ['CONTROL_PLANE_URL']
 DV_CAGE_ID = os.environ['DV_CAGE_ID']
 SECRET_MANAGER_URL = os.environ['SECRET_MANAGER_URL']
