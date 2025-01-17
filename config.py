15d81be2458ca39630215703c54c6a812264dfe3
Michiel Rogissart <michiel@datavillage.me>
2025-01-17 12:45:39 +0100

    fix data folder and add debug logs

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 
-DATA_FOLDER="data/input"
+# DATA_FOLDER="data/input"
+DATA_FOLDER="/resources/data"
 
-ALGO_VERSION = '0.4.1'
+ALGO_VERSION = '0.4.2rc1'
 CONTROL_PLANE_URL = os.environ['CONTROL_PLANE_URL']
 DV_CAGE_ID = os.environ['DV_CAGE_ID']
 SECRET_MANAGER_URL = os.environ['SECRET_MANAGER_URL']
--- write_bucket.py
+++ write_bucket.py
@@ -7,11 +7,15 @@ import os
 
 def write_data(data: dict, key_id: str, secret: str):
   data['timestamp'] = datetime.now().isoformat()
+  audit_log("creating tmp file")
   tmp_file = util.create_tmp_json(data)
+  audit_log("connecting to gcs")
   gcs_conn, duckdb_conn = gcs.connect_export(key_id, secret)
 
+  audit_log("create duckdb table")
   duckdb_conn.sql(f"CREATE TABLE export_data AS SELECT * FROM read_json('{tmp_file}')")
   
+  audit_log("export data to gcs")
   gcs_conn.export_duckdb("export_data")
   audit_log("exported data", LogLevel.INFO)
   os.remove(tmp_file)
