df4a32003e7402a200dc198e3a0f2097a1223ce6
Michiel Rogissart <michiel@datavillage.me>
2025-02-11 15:31:21 +0100

    use secret manager lib to sign data

--- decrypt.py
+++ decrypt.py
@@ -1,4 +1,4 @@
-from dv_utils import SecretManager, audit_log, LogLevel
+from dv_utils import audit_log, LogLevel
 import json
 import util
 import gcs
@@ -7,7 +7,7 @@ import io
 import requests
 import config
 
-def decrypt_file(message:dict, conn: dict):
+def decrypt_file(message:dict, location: str = None, secret_manager_key: str = None):
   file_like_obj = io.StringIO(json.dumps(message))
   files = {'message': file_like_obj}
 
@@ -20,7 +20,11 @@ def decrypt_file(message:dict, conn: dict):
   resp_json = {"content": resp_text}
   tmp_file = util.create_tmp_json(resp_json)
 
-  gcs_conn, duckdb_conn = gcs.connect_export(conn)
+  loc = location if location and len(location) else config.GCS_DEFAULT_WRITE
+  ext = "{model}.json"
+  s_key = secret_manager_key if secret_manager_key else config.SECRET_MANAGER_KEY
+
+  gcs_conn, duckdb_conn = gcs.connect(f"{loc}/{ext}", s_key)
 
   duckdb_conn.sql(f"CREATE TABLE decrypted_data AS SELECT * FROM read_json('{tmp_file}')")
 
--- write_bucket.py
+++ write_bucket.py
@@ -4,9 +4,10 @@ import gcs
 import config
 import util
 import os
+import json
+import dv_secret_manager
 
-def write_data(data: dict, location: str = None, secret_manager_key: str = None):
-  data['timestamp'] = datetime.now().isoformat()
+def write_data(data: dict, location: str = None, secret_manager_key: str = None, file_name: str = "export_data"):
   audit_log("creating tmp file")
   tmp_file = util.create_tmp_json(data)
   audit_log("connecting to gcs")
@@ -18,26 +19,32 @@ def write_data(data: dict, location: str = None, secret_manager_key: str = None)
   gcs_conn, duckdb_conn = gcs.connect(f"{loc}/{ext}", s_key)
 
   audit_log("create duckdb table")
-  duckdb_conn.sql(f"CREATE TABLE export_data AS SELECT * FROM read_json('{tmp_file}')")
+  duckdb_conn.sql(f"CREATE TABLE {file_name} AS SELECT * FROM read_json('{tmp_file}')")
   
-  audit_log("export data to gcs")
-  gcs_conn.export_duckdb("export_data")
+  audit_log("export data to gcs", LogLevel.INFO)
+  gcs_conn.export_duckdb(file_name)
   audit_log("exported data", LogLevel.INFO)
   os.remove(tmp_file)
   pass
 
 def write_signed_data(data: dict, location: str = None, secret_manager_key: str = None):
-  data['timestamp'] = datetime.now().isoformat()
-  tmp_file = util.create_tmp_json(data)
+  final_data = dict()
+  final_data['data'] = data
+  signature = __sign_data(data)
+  final_data['signature'] = signature
 
-  loc = location if location and len(location) else config.GCS_DEFAULT_WRITE
-  ext = "{model}.json"
-  s_key = secret_manager_key if secret_manager_key and len(secret_manager_key) else config.SECRET_MANAGER_KEY
-  gcs_conn, duckdb_conn = gcs.connect(f"{loc}/{ext}", s_key)
+  write_data(final_data, location, secret_manager_key, "export_data_signed")
 
-  duckdb_conn.sql(f"CREATE TABLE signed_data AS SELECT * FROM read_json('{tmp_file}')")
+def __sign_data(data: dict) -> dict:
+  data_bytes = json.dumps(data).encode("utf-8")
+  configuration = dv_secret_manager.Configuration(host = config.SECRET_MANAGER_URL)
 
-  gcs_conn.export_signed_output_duckdb("signed_data", "michiel")
-  audit_log("exported signed data", LogLevel.INFO)
-
-  os.remove(tmp_file)
+  with dv_secret_manager.ApiClient(configuration) as c:
+    inst = dv_secret_manager.DefaultApi(c)
+    try:
+      resp = inst.sign_post(data_bytes)
+      audit_log("signed data at secret manager")
+      return resp
+    except Exception as e:
+      audit_log(f"could not sign data: {e}", LogLevel.ERROR)
+      return None
