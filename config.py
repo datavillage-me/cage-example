8ae0e8d86f31061f71a22a92157b5910a49c37e7
Michiel Rogissart <michiel@datavillage.me>
2025-02-12 17:34:46 +0100

    set default secret manager key

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ ALGO_VERSION = '0.5.0rc1'
 CONTROL_PLANE_URL = os.environ['CONTROL_PLANE_URL']
 DV_CAGE_ID = os.environ['DV_CAGE_ID']
 
-SECRET_MANAGER_KEY = "configuration_example_gcs"
+SECRET_MANAGER_KEY = "configuration_datavillage"
 SECRET_MANAGER_URL = os.environ['SECRET_MANAGER_URL']
 DV_TOKEN = os.environ.get("DV_TOKEN", "")
 
