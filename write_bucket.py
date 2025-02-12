b29bcf9eba335ed88e87adcf216044c93d2d6d6a
Michiel Rogissart <michiel@datavillage.me>
2025-02-12 17:48:13 +0100

    fix write bucket event

--- write_bucket.py
+++ write_bucket.py
@@ -21,9 +21,15 @@ def write_data(data: dict, location: str = None, secret_manager_key: str = None)
   os.remove(tmp_file)
   pass
 
-def write_signed_data(data: dict, key_id: str, secret: str):
+def write_signed_data(data: dict, location: str = None, secret_manager_key: str = None):
   tmp_file = util.create_tmp_json(data)
-  gcs_conn, duckdb_conn = gcs.connect_export(key_id, secret)
+
+  loc = location if location and len(location) else config.GCS_DEFAULT_WRITE
+  ext = "{model}.json"
+  s_key = secret_manager_key if secret_manager_key and len(secret_manager_key) else config.SECRET_MANAGER_KEY
+
+  gcs_conn, duckdb_conn = gcs.connect(f"{loc}/{ext}", s_key)
+  log("connected to gcs", LogLevel.INFO)
 
   duckdb_conn.sql(f"CREATE TABLE signed_data AS SELECT * FROM read_json('{tmp_file}')")
 
